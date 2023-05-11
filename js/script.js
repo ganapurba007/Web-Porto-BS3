477fa7dccaba834d3c399155fb988508ac71b487
ganapurba007 <ganapurbakusuma86@gmail.com>
2023-05-11 22:43:13 +0700

    menambahkan gambar pada jumbtron dan edit style jumbotron

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
+//  event pada saat link diklik
 $('.page-scroll').on('click', function(e){
 
-
+	// ambil isi href
 	var tujuan = $(this).attr('href');
-
+	// tangkap elemen yang diklik
 	var elementTujuan = $(tujuan);
 
+	// pindahkan scroll
 	$('html').animate({
 		scrollTop: elementTujuan.offset().top - 50
 
