e79aa13b147ae4306d0bd22ad0cf9961b52b3e16
ganapurba007 <ganapurbakusuma86@gmail.com>
2023-05-12 06:28:33 +0700

    menambahkan parallax pada jumbotron, about, dan portofolio

--- js/script.js
+++ js/script.js
@@ -13,5 +13,45 @@ $('.page-scroll').on('click', function(e){
 	}, 1500);
 
 	e.preventDefault();
+});
 
+
+
+// parallax
+
+// about
+$(window).on('load', function(){
+	$('.pKiri').addClass('pMuncul');
+	$('.pKanan').addClass('pMuncul');
+});
+
+
+$(window).scroll(function(){
+	var wScroll = $(this).scrollTop();
+
+
+	// jumbotron
+	$('.jumbotron img').css({
+		'transform' : 'translate(0px, '+ wScroll/4.2 +'%)'
+	});
+
+	$('.jumbotron h1').css({
+		'transform' : 'translate(0px, '+ wScroll/2 +'%)'
+	});
+
+	$('.jumbotron p').css({
+		'transform' : 'translate(0px, '+ wScroll/1 +'%)'
+	});
+
+
+
+
+	// portofolio
+	if(wScroll > $('.portofolio').offset().top - 250){
+		$('.portofolio .thumbnail').each(function(i){
+			setTimeout(function(){
+				$('.portofolio .thumbnail').eq(i).addClass('muncul');
+			}, 300 * (i+1));			
+		});		
+	}
 });
